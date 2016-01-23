b6371f0d0ccd3bd7ec1907e582d827bd46f99830
Tristan Hume <tris.hume@gmail.com>
2016-01-22 23:33:08 -0500

    Add streaming websockets for data

--- public/quickchart.js
+++ public/quickchart.js
@@ -1,10 +1,10 @@
 // set up our data series with 150 random data points
 var seriesData = [ [], [], [], [], [], [], [], [], [] ];
-var random = new Rickshaw.Fixtures.RandomData(150);
-for (var i = 0; i < 150; i++) {
-  random.addData(seriesData);
+var curX = 1;
+for (var i = 0; i < seriesData.length; i++) {
+  seriesData[i].push({x: 0, y: 0});
 }
-var palette = new Rickshaw.Color.Palette( { scheme: 'classic9' } );
+var palette = new Rickshaw.Color.Palette( { scheme: 'cool' } );
 // instantiate our graph!
 var graph = new Rickshaw.Graph( {
   element: document.getElementById("chart"),
@@ -17,43 +17,43 @@ var graph = new Rickshaw.Graph( {
     {
       color: palette.color(),
       data: seriesData[0],
-      name: 'Moscow'
+      name: '0'
     }, {
       color: palette.color(),
       data: seriesData[1],
-      name: 'Shanghai'
+      name: '1'
     }, {
       color: palette.color(),
       data: seriesData[2],
-      name: 'Amsterdam'
+      name: '2'
     }, {
       color: palette.color(),
       data: seriesData[3],
-      name: 'Paris'
+      name: '3'
     }, {
       color: palette.color(),
       data: seriesData[4],
-      name: 'Tokyo'
+      name: '4'
     }, {
       color: palette.color(),
       data: seriesData[5],
-      name: 'London'
+      name: '5'
     }, {
       color: palette.color(),
       data: seriesData[6],
-      name: 'New York'
+      name: '6'
     }
   ]
 } );
 graph.render();
-var preview = new Rickshaw.Graph.RangeSlider.Preview( {
+var preview = new Rickshaw.Graph.RangeSlider( {
   graph: graph,
   element: document.getElementById('preview'),
 } );
 var hoverDetail = new Rickshaw.Graph.HoverDetail( {
   graph: graph,
   xFormatter: function(x) {
-    return new Date(x * 1000).toString();
+    return x.toString();
   }
 } );
 var annotator = new Rickshaw.Graph.Annotate( {
@@ -81,12 +81,12 @@ var smoother = new Rickshaw.Graph.Smoother( {
   element: document.querySelector('#smoother')
 } );
 var ticksTreatment = 'glow';
-var xAxis = new Rickshaw.Graph.Axis.Time( {
-  graph: graph,
-  ticksTreatment: ticksTreatment,
-  timeFixture: new Rickshaw.Fixtures.Time.Local()
-} );
-xAxis.render();
+// var xAxis = new Rickshaw.Graph.Axis.Time( {
+//   graph: graph,
+//   ticksTreatment: ticksTreatment,
+//   timeFixture: new Rickshaw.Fixtures.Time.Local()
+// } );
+// xAxis.render();
 var yAxis = new Rickshaw.Graph.Axis.Y( {
   graph: graph,
   tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
@@ -98,32 +98,52 @@ var controls = new RenderControls( {
   graph: graph
 } );
 // add some data every so often
-var messages = [
-  "Changed home page welcome message",
-  "Minified JS and CSS",
-  "Changed button color from blue to green",
-  "Refactored SQL query to use indexed columns",
-  "Added additional logging for debugging",
-  "Fixed typo",
-  "Rewrite conditional logic for clarity",
-  "Added documentation for new methods"
-];
-setInterval( function() {
-  // random.removeData(seriesData);
-  random.addData(seriesData);
-  graph.update();
-}, 500 );
-function addAnnotation(force) {
-  if (messages.length > 0 && (force || Math.random() >= 0.95)) {
-    annotator.add(seriesData[2][seriesData[2].length-1].x, messages.shift());
-    annotator.update();
-  }
-}
-addAnnotation(true);
-setTimeout( function() { setInterval( addAnnotation, 6000 ) }, 6000 );
+// var messages = [
+//   "Changed home page welcome message",
+//   "Minified JS and CSS",
+//   "Changed button color from blue to green",
+//   "Refactored SQL query to use indexed columns",
+//   "Added additional logging for debugging",
+//   "Fixed typo",
+//   "Rewrite conditional logic for clarity",
+//   "Added documentation for new methods"
+// ];
+// setInterval( function() {
+//   // random.removeData(seriesData);
+//   random.addData(seriesData);
+//   graph.update();
+// }, 500 );
+// function addAnnotation(force) {
+//   if (messages.length > 0 && (force || Math.random() >= 0.95)) {
+//     annotator.add(seriesData[2][seriesData[2].length-1].x, messages.shift());
+//     annotator.update();
+//   }
+// }
+// addAnnotation(true);
+// setTimeout( function() { setInterval( addAnnotation, 6000 ) }, 6000 );
 // var previewXAxis = new Rickshaw.Graph.Axis.Time({
 //   graph: preview.previews[0],
 //   timeFixture: new Rickshaw.Fixtures.Time.Local(),
 //   ticksTreatment: ticksTreatment
 // });
 // previewXAxis.render();
+
+function connect(room) {
+  socket = new WebSocket("ws://127.0.0.1:8080/ws?room="+encodeURIComponent(room));
+
+  socket.onmessage = function(jsonMessage) {
+    var msg = JSON.parse(jsonMessage.data);
+    console.log(msg);
+    for (var i = 0; i < seriesData.length; i++) {
+      seriesData[i].push({x: curX, y: msg[i]});
+    };
+    curX++;
+    graph.update();
+  }
+
+  socket.onclose = function() {
+    console.log("socket closed - reconnecting...");
+    window.setTimeout(connect,1000);
+  }
+}
+connect("main");
--- source/app.d
+++ source/app.d
@@ -1,8 +1,54 @@
 import vibe.d;
+import std.random;
 
-shared static this()
-{
+enum numSeries = 7;
+
+struct Room {
+  WebSocket socket;
+  int[numSeries] curVals;
+
+  this(WebSocket sock) {
+    socket = sock;
+    foreach(ref x; curVals) {
+      x = 0;
+    }
+  }
+
+  void sendData() {
+    auto json = serializeToJsonString(curVals);
+    socket.send(json);
+  }
+}
+private Room[string] rooms;
+
+final class QuickChart {
+  void getWS(string room, scope WebSocket socket) {
+    rooms[room] = Room(socket);
+
+    while (socket.waitForData) {
+      if (!socket.connected) break;
+      auto text = socket.receiveText;
+      logInfo("Received: \"%s\" from %s.", text, room);
+    }
+
+    socket.close;
+    rooms.remove(room);
+  }
+}
+
+void testData() {
+  if(!("main" in rooms)) return;
+  auto room = rooms["main"];
+
+  room.curVals[0] = uniform(0,100);
+  room.curVals[1] = uniform(20,50);
+  room.curVals[2] = uniform(0,20);
+  room.sendData();
+}
+
+shared static this() {
   auto router = new URLRouter;
+  router.registerWebInterface(new QuickChart);
   router.get("*", serveStaticFiles("./public/"));
 
 	auto settings = new HTTPServerSettings;
@@ -10,9 +56,6 @@ shared static this()
 	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-}
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.writeBody("Hello, World!");
+  setTimer(msecs(500), toDelegate(&testData), true);
 }
